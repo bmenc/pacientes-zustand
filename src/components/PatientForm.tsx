520d0eacaeb76753d5c0ede578be22f68c81cbbb
bmenc <bmenchaca.cca@gmail.com>
2026-02-14 18:14:05 -0600

    242. Definiendo un Type al Formulario

--- src/components/PatientForm.tsx
+++ src/components/PatientForm.tsx
@@ -1,12 +1,13 @@
 import { useForm } from 'react-hook-form';
 import Error from './Error';
+import type { DraftPatient } from '../types';
 
 export default function PatientForm() {
 
-  const { register, handleSubmit, formState: { errors } } = useForm()
+  const { register, handleSubmit, formState: { errors } } = useForm<DraftPatient>()
 
-  const registerPatient = () => {
-    console.log("registerPatient");
+  const registerPatient = (data: DraftPatient) => {
+    console.log(data);
   }
 
   return (
@@ -39,7 +40,7 @@ export default function PatientForm() {
 
           {errors.name &&
             <Error>
-              {errors.name?.message?.toString()}
+              {errors.name?.message}
             </Error>
           }
         </div>
@@ -59,7 +60,7 @@ export default function PatientForm() {
           />
           {errors.caretaker &&
             <Error>
-              {errors.caretaker?.message?.toString()}
+              {errors.caretaker?.message}
             </Error>
           }
         </div>
@@ -83,7 +84,7 @@ export default function PatientForm() {
           />
           {errors.email &&
             <Error>
-              {errors.email?.message?.toString()}
+              {errors.email?.message}
             </Error>
           }
         </div>
@@ -102,7 +103,7 @@ export default function PatientForm() {
           />
           {errors.date &&
             <Error>
-              {errors.date?.message?.toString()}
+              {errors.date?.message}
             </Error>
           }
         </div>
@@ -121,7 +122,7 @@ export default function PatientForm() {
           ></textarea>
           {errors.symptoms &&
             <Error>
-              {errors.symptoms?.message?.toString()}
+              {errors.symptoms?.message}
             </Error>
           }
         </div>
