0b55a4bea2c84f5c7f79b54eb569c7c5f2ee2b4b
bmenc <bmenchaca.cca@gmail.com>
2026-02-14 18:53:32 -0600

    244. Creando el Store de Zustand

---
+++ src/store.ts
@@ -0,0 +1,11 @@
+import { create } from 'zustand'
+import type { Patient } from './types'
+
+type PatientState = {
+  patients: Patient[]
+}
+
+export const usePatientStore = create<PatientState>(() => ({
+  patients: []
+
+})) 
